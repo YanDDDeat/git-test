6400c7388525ad2b6d542a71a4b3376b08992f12
YanDDDeat <490299705@qq.com>
2022-03-22 15:02:20 +0800

    second commit

---
+++ src/main/java/com/yan/git/HelloGit2.java
@@ -0,0 +1,11 @@
+package com.yan.git;
+
+/**
+ * @author Yyy
+ * @creat2022-03-2215:01
+ */
+public class HelloGit2 {
+    public static void main(String[] args) {
+        System.out.println("hello git2");
+    }
+}
