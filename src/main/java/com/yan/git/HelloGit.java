90aeaa2411110029dfe39b796b065f90cf78ed24
YanDDDeat <102040993+YanDDDeat@users.noreply.github.com>
2022-03-24 14:58:59 +0800

    Update HelloGit.java

--- src/main/java/com/yan/git/HelloGit.java
+++ src/main/java/com/yan/git/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("success!!");
+        System.out.println("Test Pull!!");
     }
 }
